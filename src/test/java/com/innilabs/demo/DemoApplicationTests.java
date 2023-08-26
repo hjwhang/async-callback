e9090d8594c4b5597b44ed0e3f7ff228fc76f46a
Hoonjoo Whang <hjwhang@gmail.com>
2023-08-26 23:21:17 +0900

    Initial Commit

---
+++ src/test/java/com/innilabs/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innilabs.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
